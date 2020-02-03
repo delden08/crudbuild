28f527ee76df24ad9c621c3ab250652c618e25ac
Sander <delden08@gmail.com>
2020-02-03 22:19:36 +0100

    beta version node and react connection

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect} from 'react';
+import ReactDom from 'react-dom';
+import axios from 'axios';
 
 function App() {
+  useEffect(() => {
+    axios.get('http://localhost:3000/users')
+     .then(response => {
+       console.log(response.data);
+     })
+   })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <h1>
+      Welcome to React
+    </h1>
   );
 }
 
-export default App;
+// export default App;
+
+ReactDOM.render(<App />, document.getElementById('root'));
